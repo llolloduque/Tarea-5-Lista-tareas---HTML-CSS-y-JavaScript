5528a60477099591975fc36d1f065c9436cc0480
Adrian_duque <adrianduquebonilla@gmail.com>
2026-03-25 11:31:50 +0000

    dejando de css a  mi gusto

--- scrip.js
+++ scrip.js
@@ -1,39 +1,34 @@
-// Esta función se ejecuta cuando haces click en "Añadir"
+// Esta función Añadir
 function agregarTarea(){
 
-  // 1. Cogemos el input (donde escribes)
+  //  textito
   let entrada = document.getElementById("entradaTarea");
 
-  // 2. Guardamos el texto que escribió el usuario
   let texto = entrada.value;
-
-  // 3. Si está vacío, no hacemos nada (porque añadir tareas vacías es inútil )
+  
   if(texto === "") return;
 
-  // 4. Creamos un <li> (una tarea nueva en la lista)
+  // lista
   let elementoLista = document.createElement("li");
 
-  // Le ponemos el texto que escribió el usuario
   elementoLista.textContent = texto;
 
-  // 5. Creamos el botón de borrar (la X malvada )
+  //  botones
   let botonBorrar = document.createElement("button");
 
   botonBorrar.textContent = "X";
   botonBorrar.className = "borrar";
 
-  // 6. Cuando haces click en el botón...
+  //  eliminar
   botonBorrar.onclick = function(){
-    // ...elimina la tarea completa (adiós)
     elementoLista.remove();
   };
 
-  // 7. Metemos el botón dentro del <li>
   elementoLista.appendChild(botonBorrar);
 
-  // 8. Añadimos el <li> a la lista (ul)
+  // Añadir a la lista
   document.getElementById("listaTareas").appendChild(elementoLista);
 
-  // 9. Limpiamos el input para escribir otra tarea
+  
   entrada.value = "";
 }
